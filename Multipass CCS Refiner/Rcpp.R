582f4812fe95c6516e2a2701c43e2cf45b0123ab
Facundo Fernandez <facundo.fernandez@chemistry.gatech.edu>
2026-05-20 12:36:26 -0400

    Delete Multipass CCS Refiner/Rcpp.R
    Remove packages.R - dependency resolved in app.R - 20May26

--- Multipass CCS Refiner/Rcpp.R
+++
@@ -1,3 +0,0 @@
-# When publishing app to Shiny.io Rcpp is sometimes not compatible.
-# This script ensures that the latest version is installed and the application is publishable.
-install.packages("Rcpp", type = "binary")
